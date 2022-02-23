d5416e2afb6753ca6a13cf50765c07f59739074a
LuckyLoui-C <67573202+LuckyLoui-C@users.noreply.github.com>
2022-02-23 17:19:33 +0800

    Added very simple main menu
    Play button and exit button.
    Loads scene on button press.

---
+++ RGP Assessment 1/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    // Now any scene that has a button for scene changing can just access this and call LoadScene and tell it the name of the scene it wants to got to
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(WaitBeforeLoad(sceneName));
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+    private IEnumerator WaitBeforeLoad(string sceneName)
+    {
+        yield return new WaitForSeconds(0.1f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
